c2ff2ebd44ca6d61f6bf8d5b86efc8065ca48c84
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-02-13 10:55:39 +0200

    Added menu icon

--- About.js
+++ About.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Dimensions, Button, StatusBar } from 'react-native';
+import Hamburger from 'react-native-hamburger';
 
 export default class About extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            active: true
+        }
+    }
     render() {
         return (
             <View style={styles.container}>
+                <Hamburger 
+                    active={this.state.active}
+                    type="cross"
+                    onPress={() => this.setState({active: !this.state.active})}
+                />
                 <StatusBar hidden={true}/>
                 <Text>This is infoooo</Text>
             </View>
--- Loading.js
+++ Loading.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, Dimensions, Button, StatusBar } from 'react-native';
 
 export default class Loading extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isLoading: false
-        }
-    }
     render() {
         return(
             <View style={styles.container}>
