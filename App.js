34ff18ab675b187db7316eafb9d8af0a4c97731c
Gerda Šimoliūnaitė <sml.gerda@gmail.com>
2018-04-10 08:50:42 +0300

    Deleted loading screen, instead created new splash screen

--- App.js
+++ App.js
@@ -2,23 +2,9 @@ import React, {Component} from 'react';
 import Router from './routes';
 import {View, AppRegistry } from 'react-native';
 import { Asset, AppLoading } from 'expo';
-import Loading from './Loading';
 
 export default class App extends Component {
-  state = {
-    isReady: true,
-  };
-
-  componentDidMount() {
-    setTimeout(() => this.setState({isReady: false}), 3000);
-  }
   render () {
-    if (this.state.isReady) {
-      return (
-        <Loading/>
-      );
-    }
-      
       return (
         <Router/>
       );
